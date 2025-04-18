4ce2ef22fb53f298032a0dd3277afecb8b8806b4
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-04-19 03:49:52 +0600

    common components for auth pages

--- team-trakking-frontend/src/pages/Login/sign-in.tsx
+++ team-trakking-frontend/src/pages/Login/sign-in.tsx
@@ -1,22 +1,14 @@
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@/components/ui/form';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { FcGoogle } from 'react-icons/fc';
-import { Mail, LockKeyhole } from 'lucide-react';
+import { Form } from '@/components/ui/form';
+import { LockKeyhole, Mail } from 'lucide-react';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useNavigate } from 'react-router-dom';
 import { AuthLayout } from './components/auth-layout.tsx';
+import { AuthCard } from './components/auth-card';
+import { FormInputField } from './components/form-input.tsx';
 
 // Define form schema with Zod
 const loginSchema = z.object({
@@ -37,99 +29,51 @@ export const Login = () => {
     },
   });
 
-  const onSubmit = (values: { email: string; password: string }) => {
-    console.log(values);
+  const onSubmit = () => {
     navigate('/home');
   };
 
   return (
     <AuthLayout isLoginPage={true}>
       {/* MAIN => Card */}
-      <div className="flex justify-center">
-        <Card className="lg:w-[480px] w-[360px] border-none">
-          <CardHeader className={'justify-center'}>
-            <CardTitle className="text-4xl font-bold">Welcome back!</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <Button
-              variant="outline"
-              className="w-full border-gray-300 text-base font-medium gap-2"
-            >
-              <FcGoogle size={20} />
-              Continue with Google
-            </Button>
+      <AuthCard title="Welcome back!" onGoogleClick={() => {}}>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormInputField
+              control={form.control}
+              name="email"
+              label="Email"
+              placeholder="Enter your work email"
+              icon={Mail}
+            />
 
-            <div className="w-full flex items-center">
-              <div className="flex-grow h-px bg-gray-200" />
-              <span className="mx-2 text-sm text-gray-300">OR</span>
-              <div className="flex-grow h-px bg-gray-200" />
-            </div>
-
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-4"
-              >
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label>Email</Label>
-                      <div className="relative">
-                        <Mail className="absolute left-3 top-3 h-4 w-4 text-gray-400" />
-                        <FormControl>
-                          <Input
-                            placeholder="Enter your work email "
-                            className="pl-10 py-5 placeholder:text-base placeholder:text-gray-400"
-                            {...field}
-                          />
-                        </FormControl>
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label>Password</Label>
-                      <div className="relative">
-                        <LockKeyhole className="absolute left-3 top-3 h-4 w-4 text-gray-400" />
-                        <FormControl>
-                          <Input
-                            type={showPassword ? 'text' : 'password'}
-                            placeholder="Enter your password"
-                            className="pl-10 py-5 placeholder:text-base placeholder:text-gray-400"
-                            {...field}
-                          />
-                        </FormControl>
-                        <Button
-                          variant="link"
-                          className="absolute right-3 text-normal top-1 hover:decoration-dashed text-indigo-600"
-                          onClick={() => setShowPassword(!showPassword)}
-                        >
-                          Forgot Password
-                        </Button>
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+            <FormInputField
+              control={form.control}
+              name="password"
+              label="Password"
+              placeholder="Enter your password"
+              icon={LockKeyhole}
+              type={showPassword ? 'text' : 'password'}
+              rightElement={
                 <Button
-                  type={'submit'}
-                  className="w-full rounded-lg h-[50px] font-bold text-md bg-indigo-600 hover:bg-indigo-700"
+                  variant="link"
+                  className="absolute right-3 text-normal top-1 hover:decoration-dashed text-indigo-600"
+                  onClick={() => setShowPassword(!showPassword)}
                 >
-                  Log In
+                  Forgot Password
                 </Button>
-              </form>
-            </Form>
-          </CardContent>
-        </Card>
-      </div>
+              }
+            />
+
+            <Button
+              type="submit"
+              className="w-full rounded-lg h-[50px] font-bold text-md bg-indigo-600 hover:bg-indigo-700"
+            >
+              Log In
+            </Button>
+          </form>
+        </Form>
+      </AuthCard>
     </AuthLayout>
   );
 };
--- team-trakking-frontend/src/pages/Login/sign-up.tsx
+++ team-trakking-frontend/src/pages/Login/sign-up.tsx
@@ -1,22 +1,14 @@
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@/components/ui/form';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
-import { FcGoogle } from 'react-icons/fc';
-import { Mail, LockKeyhole, User } from 'lucide-react';
+import { Form } from '@/components/ui/form';
+import { LockKeyhole, Mail, User } from 'lucide-react';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useNavigate } from 'react-router-dom';
 import { AuthLayout } from './components/auth-layout.tsx';
+import { AuthCard } from './components/auth-card.tsx';
+import { FormInputField } from './components/form-input.tsx';
 
 // Define form schema with Zod - now including name field
 const loginSchema = z.object({
@@ -39,128 +31,59 @@ export const SignUp = () => {
     },
   });
 
-  const onSubmit = (values: {
-    name: string;
-    email: string;
-    password: string;
-  }) => {
-    console.log(values);
+  const onSubmit = () => {
     navigate('/home');
   };
 
   return (
     <AuthLayout isLoginPage={false}>
-      {/* MAIN => Card */}
-      <div className="flex justify-center">
-        <Card className="lg:w-[480px] w-[360px] border-none">
-          <CardHeader className={'justify-center'}>
-            <CardTitle className="text-4xl font-bold">
-              Seconds to sign up!
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <Button
-              variant="outline"
-              className="w-full border-gray-300 text-base font-medium gap-2"
-            >
-              <FcGoogle size={20} />
-              Continue with Google
-            </Button>
+      <AuthCard title="Seconds to sign up!" onGoogleClick={() => {}}>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormInputField
+              control={form.control}
+              name="name"
+              label="Full Name"
+              placeholder="John Doe"
+              icon={User}
+            />
 
-            <div className="w-full flex items-center">
-              <div className="flex-grow h-px bg-gray-200" />
-              <span className="mx-2 text-sm text-gray-300">OR</span>
-              <div className="flex-grow h-px bg-gray-200" />
-            </div>
+            <FormInputField
+              control={form.control}
+              name="email"
+              label="Email"
+              placeholder="example@site.com"
+              icon={Mail}
+            />
 
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-4"
-              >
-                {/* Added Name field */}
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label>Full Name</Label>
-                      <div className="relative">
-                        <User className="absolute left-3 top-3 h-4 w-4 text-gray-400" />
-                        <FormControl>
-                          <Input
-                            placeholder="John Doe"
-                            className="pl-10 py-5 placeholder:text-base placeholder:text-gray-400"
-                            {...field}
-                          />
-                        </FormControl>
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label>Email</Label>
-                      <div className="relative">
-                        <Mail className="absolute left-3 top-3 h-4 w-4 text-gray-400" />
-                        <FormControl>
-                          <Input
-                            placeholder="example@site.com"
-                            className="pl-10 py-5 placeholder:text-base placeholder:text-gray-400"
-                            {...field}
-                          />
-                        </FormControl>
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label>Password</Label>
-                      <div className="relative">
-                        <LockKeyhole className="absolute left-3 top-3 h-4 w-4 text-gray-400" />
-                        <FormControl>
-                          <Input
-                            type={showPassword ? 'text' : 'password'}
-                            placeholder="Minimum 8 characters"
-                            className="pl-10 py-5 placeholder:text-base placeholder:text-gray-400"
-                            {...field}
-                          />
-                        </FormControl>
-                        <Button
-                          variant="link"
-                          type={'button'}
-                          className="absolute right-3 text-normal top-1 hover:decoration-dashed text-indigo-600"
-                          onClick={() => setShowPassword(!showPassword)}
-                        >
-                          {!showPassword ? 'Show' : 'Hide'}
-                        </Button>
-                      </div>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+            <FormInputField
+              control={form.control}
+              name="password"
+              label="Password"
+              placeholder="Minimum 8 characters"
+              icon={LockKeyhole}
+              type={showPassword ? 'text' : 'password'}
+              rightElement={
                 <Button
-                  type={'submit'}
-                  className="w-full rounded-lg h-[50px] font-bold text-md bg-indigo-600 hover:bg-indigo-700"
+                  variant="link"
+                  type="button"
+                  className="absolute right-3 text-normal top-1 hover:decoration-dashed text-indigo-600"
+                  onClick={() => setShowPassword(!showPassword)}
                 >
-                  Sign up
+                  {!showPassword ? 'Show' : 'Hide'}
                 </Button>
-              </form>
-            </Form>
-          </CardContent>
-        </Card>
-      </div>
+              }
+            />
+
+            <Button
+              type="submit"
+              className="w-full rounded-lg h-[50px] font-bold text-md bg-indigo-600 hover:bg-indigo-700"
+            >
+              Sign up
+            </Button>
+          </form>
+        </Form>
+      </AuthCard>
     </AuthLayout>
   );
 };
