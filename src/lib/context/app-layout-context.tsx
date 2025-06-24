36df2e95a27c72430dc011ca24e2252ead3e43b6
Jawahiir Nabhan <jawahiir.dcastalia@gmail.com>
2025-06-24 14:25:38 +0600

    App Layout context added for data persistency.

---
+++ src/lib/context/app-layout-context.tsx
@@ -0,0 +1,31 @@
+// src/lib/context/app-layout-context.tsx
+import { createContext, useContext, ReactNode } from 'react';
+import { SpaceGlobal } from '@/types/request-response/space/ApiResponse';
+
+interface AppContextType {
+  spaceGlobal: SpaceGlobal | null;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export const AppContextProvider = ({
+  children,
+  spaceGlobal,
+}: {
+  children: ReactNode;
+  spaceGlobal: SpaceGlobal | null;
+}) => {
+  return (
+    <AppContext.Provider value={{ spaceGlobal }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = (): AppContextType => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error('useAppContext must be used within an AppContextProvider');
+  }
+  return context;
+};
