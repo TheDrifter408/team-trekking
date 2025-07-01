89451edb6ee415f1e14b2f26a11674b42f1bff67
Mehedi Hassan Piash <piash599@gmail.com>
2025-07-01 17:05:48 +0600

    - Fixing missing getRandomHexColor

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -123,3 +123,7 @@ export const handleMutation = async <T>(
     return { error };
   }
 };
+export const getRandomHexColor = (): string =>
+  `#${Math.floor(Math.random() * 0xffffff)
+    .toString(16)
+    .padStart(6, '0')}`;
